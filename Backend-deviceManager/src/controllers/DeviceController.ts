092dd9ff0a2cc834bb8f6414ac1ca8e3615e9ecb
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-04-05 15:22:37 +0530

    set up deviceController.ts

---
+++ Backend-deviceManager/src/controllers/DeviceController.ts
@@ -0,0 +1,18 @@
+export const createLocation = async(req: any,  res:any) =>{
+
+}
+
+export const updateLocation = async(req: any, res:any)=>{
+
+}
+export const deleteLocation = async(req: any,  res:any) =>{
+
+}
+
+export const viewLocation = async(req: any, res:any)=>{
+    
+}
+
+export const viewAllLocation = async(req: any, res:any)=>{
+    
+}
---
+++ Backend-deviceManager/src/controllers/LoactionController.ts
@@ -0,0 +1,18 @@
+export const createDevice = async(req: any,  res:any) =>{
+
+}
+
+export const updateDevice = async(req: any, res:any)=>{
+
+}
+export const deleteDevice = async(req: any,  res:any) =>{
+
+}
+
+export const viewDevice = async(req: any, res:any)=>{
+    
+}
+
+export const viewAllDevice = async(req: any, res:any)=>{
+    
+}
