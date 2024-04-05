d0fa54fea8972d4594208974f81606797c2144e3
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-04-05 23:14:56 +0530

    upadte location ruters

--- Backend-deviceManager/src/controllers/DeviceController.ts
+++ Backend-deviceManager/src/controllers/DeviceController.ts
@@ -1,18 +1,18 @@
-export const createLocation = async(req: any,  res:any) =>{
+export const createDevice = async(req: any,  res:any) =>{
 
 }
 
-export const updateLocation = async(req: any, res:any)=>{
+export const updateDevice = async(req: any, res:any)=>{
 
 }
-export const deleteLocation = async(req: any,  res:any) =>{
+export const deleteDevice = async(req: any,  res:any) =>{
 
 }
 
-export const viewLocation = async(req: any, res:any)=>{
+export const viewDevice = async(req: any, res:any)=>{
     
 }
 
-export const viewAllLocation = async(req: any, res:any)=>{
+export const viewAllDevice = async(req: any, res:any)=>{
     
 }
---
+++ Backend-deviceManager/src/routes/LocationRouters.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import * as LocationController from "../controllers/LoactionController"
+import * as VerifyToken from "../middlewares/verifyToken"
+
+
+let router = express.Router();
+
+router.post('/save',VerifyToken.verifyToken, LocationController.createLocation) //location /save
+router.put('/update',VerifyToken.verifyToken, LocationController.updateLocation) //location /update
+router.post('/delete/:locationId',VerifyToken.verifyToken, LocationController.deleteLocation) //location /delete
+router.get('/get/location',VerifyToken.verifyToken, LocationController.viewLocation) //location /get/location?title= 
+router.get('/get/all', VerifyToken.verifyToken, LocationController.viewAllLocation) // note/get/all
+
+
+export default router;
