0ab544a13995be3113740f2421b09e6a70758cbc
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-04-05 23:21:48 +0530

    deviceRouters.ts updated

--- Backend-deviceManager/src/routes/DeviceRoutes.ts
+++ Backend-deviceManager/src/routes/DeviceRoutes.ts
@@ -1,3 +1,15 @@
 import express from "express";
 import * as DeviceController from "../controllers/DeviceController"
+import * as VerifyToken from "../middlewares/verifyToken"
 
+
+let router = express.Router();
+
+router.post('/save',VerifyToken.verifyToken, DeviceController.createDevice) //device /save
+router.put('/update',VerifyToken.verifyToken, DeviceController.updateDevice) //device /update
+router.post('/delete/:deviceId',VerifyToken.verifyToken, DeviceController.deleteDevice) //device /delete
+router.get('/get/device',VerifyToken.verifyToken, DeviceController.viewDevice) //device /get/location?title= 
+router.get('/get/all', VerifyToken.verifyToken, DeviceController.viewAllDevice) // device/get/all
+
+
+export default router;
