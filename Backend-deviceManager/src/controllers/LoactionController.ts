d8e9073836ffcc212211b815e69f0524e6916377
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-04-06 00:25:28 +0530

    create Loaction

--- Backend-deviceManager/src/controllers/LoactionController.ts
+++ Backend-deviceManager/src/controllers/LoactionController.ts
@@ -1,5 +1,39 @@
+import { CustomResponse } from "../dtos/CustomerResponse"
+import LocationModel from "../models/LocationModels"
+import { LocationInterface } from "../type/SchemaTypes"
+
+
 export const createLocation = async(req: any,  res:any) =>{
 
+    try{
+
+        let locationModel = new LocationModel({
+            name:req.body.name,
+            address:req.body.address,
+            phone:req.body.phone,
+            device:req.body.device
+        });
+
+        await locationModel.save()
+            .then( success => {
+            res.status(200).send(
+                new CustomResponse(200,"Note saved successfully.",success)
+            )
+        })
+
+            .catch( error => {
+                res.status(500).send(
+                    new CustomResponse(500,`Something went wrong! : ${error}`)
+                )
+            })
+
+
+
+    }catch(error){
+        res.status(500).send(
+            new CustomResponse(500,`Error : ${error}`)
+        )
+    }
 }
 
 export const updateLocation = async(req: any, res:any)=>{
