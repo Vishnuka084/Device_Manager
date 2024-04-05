ab2d6c583dace63ee9e6730a0d054c77e8b4e1b8
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-04-05 23:52:45 +0530

    Uploadpic  device router

--- Backend-deviceManager/src/routes/DeviceRoutes.ts
+++ Backend-deviceManager/src/routes/DeviceRoutes.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import * as DeviceController from "../controllers/DeviceController"
 import * as VerifyToken from "../middlewares/verifyToken"
-
+import {upload, uploadPic} from "../middlewares/imageUpLoader"
 
 let router = express.Router();
 
 router.post('/save',VerifyToken.verifyToken, DeviceController.createDevice) //device /save
-router.put('/update',VerifyToken.verifyToken, DeviceController.updateDevice) //device /update
+router.put('/update',VerifyToken.verifyToken,uploadPic.single('file'), DeviceController.updateDevice) //device /update
 router.post('/delete/:deviceId',VerifyToken.verifyToken, DeviceController.deleteDevice) //device /delete
 router.get('/get/device',VerifyToken.verifyToken, DeviceController.viewDevice) //device /get/location?title= 
 router.get('/get/all', VerifyToken.verifyToken, DeviceController.viewAllDevice) // device/get/all
