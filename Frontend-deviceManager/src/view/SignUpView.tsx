5ebfcad26d1eb3fec38499ffb3672b04942a385b
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-04-07 18:00:18 +0530

    create variable chechkedvalues function

--- Frontend-deviceManager/src/view/SignUpView.tsx
+++ Frontend-deviceManager/src/view/SignUpView.tsx
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import * as Validator from "../util/validator.ts";
 
 import {useNavigate} from "react-router-dom";
 
@@ -19,12 +20,39 @@ function SignUpView(){
     let navigate = useNavigate();
 
 
+
+
     const handleInput = (e:any, type:string):void =>{
         switch (type){
             case 'name':
                 setName(e.target.value)
-
+                setIsValidName(Validator.fullNameValidator(e.target.value))
+                break;
+            case 'email':
+                setEmail(e.target.value)
+                setIsValidEmail(Validator.emailValidator(e.target.value))
+                break;
+            case 'password':
+                setPassword(e.target.value)
+                setIsValidPassword(Validator.passwordValidator(e.target.value))
+                break;
+            case 'rePassword':
+                setPassword(e.target.value)
+                setIsMachedPassword(machedPassword(e.target.value))
         }
+
+        function machedPassword( rePassword: string) :boolean{
+            return password === rePassword;
+        }
+
+        function checkValues() {
+
+            let name_valid = Validator.fullNameValidator(name);
+            let email_valid= Validator.emailValidator(email);
+            let password_valid= Validator.passwordValidator(password);
+            let mached_password = machedPassword(rePassword);
+        }
+
     }
 
 
