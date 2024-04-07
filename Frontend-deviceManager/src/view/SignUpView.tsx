96394e8f56ee464bd37a3ef56aebd5b340877fe7
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-04-07 17:44:14 +0530

    add variable signupview

--- Frontend-deviceManager/src/view/SignUpView.tsx
+++ Frontend-deviceManager/src/view/SignUpView.tsx
@@ -1,7 +1,24 @@
-
+import Input from "../components/input/input.tsx";
+import {useState} from "react";
+import Swal from "sweetalert2";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 function SignUpView(){
 
-    const
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [rePassword, setRePassword] = useState("")
+    const [isShow, setIsShow] = useState(false)
+
+    const [isValidName, setIsValidName] = useState(true)
+    const [isValidEmail, setIsValidEmail] = useState(true)
+    const [isValidPassword, setIsValidPassword] = useState(true)
+    const [isMachedPassword, setIsMachedPassword] = useState(true)
+
+
+    let navigate = useNavigate();
+
 
 }
